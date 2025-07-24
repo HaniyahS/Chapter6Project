41bccedeecb663c57278304cf1ee6be9f2c50b2d
HaniyahS <lionroar87453@gmail.com>
2025-07-23 21:39:04 -0400

    star

--- Chapter6Project/Chapter6Project.cpp
+++ Chapter6Project/Chapter6Project.cpp
@@ -6,27 +6,106 @@
 using namespace std;
 
 //Function Prototype
-double getCelsius(int);
+void getScore(double &);
+double calcAverage(double, double, double, double, double);
+double isLower(double, double, double, double, double);
+double isHigher(double, double, double, double, double);
 
+int i = 1;
 int main() {
-    //Global constants
-    const int MIN = 0 , MAX = 20;
+   
+    double score1, score2, score3, score4, score5;
+   
+    getScore(score1);
+    i++;
+    getScore(score2);
+    i++;
+    getScore(score3);
+    i++;
+    getScore(score4);
+    i++;
+    getScore(score5);
 
-    //Setting up table and loop
-    cout << "Farenheit 0 - 20 to Celsius Temperature Table\n\nFarenheit             Celsius";
-    for (int F = MIN; F <= MAX; F++) {
+    double sum = score1 + score2 + score3 + score4 + score5;
+    cout << "\nContestant's score: " << sum;
+    cout << calcAverage(score1, score2, score3, score4, score5);
+    cout << endl;
 
-        cout << "\n " << setw(2) << F;
-        cout << " " << setw(23) << getCelsius(F);
-        cout << endl;
-    }
     return 0;
 }
 
 //Function definition
-double getCelsius(int F) {
+void getScore(double &score) {
+  
+    cout << "\nEnter judge " << i << "'s score: ";
+    
+        cin >> score;
+        
 
-    return (5 * (F - 32)) / 9;
+        while (score < 0 || score > 10) {
+            cout << "\nThe score cannot be less than 0 or over 10";
+        }
 
 }
 
+double calcAverage(double score1, double score2, double score3, double score4, double score5) {
+    double lowest = isLower(score1, score2, score3, score4, score5);
+    double highest = isHigher(score1, score2, score3, score4, score5);
+    double average, sum = score1 + score2 + score3 + score4 + score5;
+
+    sum -= lowest;
+    sum -= highest;
+
+    average = sum / 3;
+
+    cout << endl;
+    cout << "\nlowest = " << lowest << "\nhighest = " << highest << "\naverage = " << average;
+    return average;
+}
+
+double isHigher(double score1, double score2, double score3, double score4, double score5) {
+
+    if (score1 > score2 && score1 > score3 && score1 > score4 && score1 > score5) {
+        return score1;
+
+    }
+    else if (score2 > score1 && score2 > score3 && score2 > score4 && score2 > score5) {
+        return score2;
+
+    }
+    else if (score3 > score1 && score3 > score2 && score3 > score4 && score3 > score5) {
+        return score3;
+
+    }
+    else if (score4 > score1 && score4 > score2 && score4 > score3 && score4 > score5) {
+        return score4;
+    }
+    else {
+        return score5;
+    }
+
+}
+
+double isLower(double score1, double score2, double score3, double score4, double score5) {
+
+    if (score1 < score2 && score1 < score3 && score1 < score4 && score1 < score5) {
+        return score1;
+
+    }
+    else if(score2 < score1 && score2 < score3 && score2 < score4 && score2 < score5){
+        return score2;
+
+    }
+    else if (score3 < score1 && score3 < score2 && score3 < score4 && score3 < score5) {
+        return score3;
+
+    }
+    else if(score4 < score1 && score4 < score2 && score4 < score3 && score4 < score5){
+        return score4;
+
+    }
+    else {
+        return score5;
+    }
+
+}
