87719b20692be014a15d6e760f2476dddc38d28a
HaniyahS <lionroar87453@gmail.com>
2025-07-28 16:39:22 -0400

    fixed

--- Chapter6Project/Chapter6Project.cpp
+++ Chapter6Project/Chapter6Project.cpp
@@ -6,77 +6,75 @@
 
 using namespace std;
 
-//Function Prototype
-void getRegInfo(string& Region , int& accidents);
-void showsLowest(string Region , int accidents);
+// Function prototype - now takes minRegion and minAccidents as references
+void getRegInfo(string& Region, int& accidents, string& minRegion, int& minAccidents);
+void showsLowest(string Region, int accidents);
 bool isLower(int newValue, int currentLowest);
 
-// Variables
-int accidents, minAccidents;
-int numRegions;
-string Region, minRegion;
-
 int main() {
-    int g = 1;
+    // Variables
+    int accidents;
+    int numRegions;
+    string Region;
+
+    // Local not global
+    string minRegion;
+    int minAccidents;
+
     cout << "Enter # of regions: ";
     cin >> numRegions;
 
-    //Getting Region info
-    //Intialize
-   getRegInfo(minRegion, minAccidents);
-   g++;
+    // Getting Region info
+    // Initializing minRegion and minAccidents with the first region's data
+    getRegInfo(Region, accidents, minRegion, minAccidents);
 
-   for (int i = 1; i < numRegions; i++) {
-       getRegInfo(Region, accidents);
+    // Assigning the first region's data to minRegion and minAccidents
+    minRegion = Region;
+    minAccidents = accidents;
 
-       if (isLower(accidents, minAccidents)) {
-           minRegion = Region;
-           minAccidents = accidents;
-       }
-       
-   }
+    for (int i = 1; i < numRegions; i++) {
+
+        // Passes minRegion and minAccidents to getRegInfo
+        getRegInfo(Region, accidents, minRegion, minAccidents);
+
+        if (isLower(accidents, minAccidents)) {
+            minRegion = Region;
+            minAccidents = accidents;
+        }
+    }
+
     
-   
-
-    // Calls function to find which region had the lowest amount.
-    showsLowest(minRegion , minAccidents);
+    showsLowest(minRegion, minAccidents);
 
     return 0;
 }
 
 // Asks for number of accidents and validates input.
-void getRegInfo(string& Region , int& accidents) {
-    
-    
-    
-    
-        cout << "\nEnter name of Region " << ": ";
-        cin >> Region;
-        
-        minRegion = Region;
+void getRegInfo(string& Region, int& accidents, string& minRegionRef, int& minAccidentsRef) { // Takes minRegion and minAccidents as references
+    cout << "\nEnter name of Region: ";
+    cin >> Region;
 
-        cout << "\nEnter the # of accidents for Region " << Region << ": ";
+    cout << "\nEnter the # of accidents for Region " << Region << ": ";
+    cin >> accidents;
+
+    while (accidents < 0) {
+        cout << "\n# of accidents cannot be less than 0.\nTry again: ";
         cin >> accidents;
-
-        while (accidents < 0) {
-            cout << "\n# of accidents cannot be less than 0.\nTry again: ";
-            cin >> accidents;
-
     }
-    
 }
 
-// Function to compare accident numbers and return true if new is lower
+
 bool isLower(int newValue, int currentLowest) {
     return newValue <= currentLowest;
 }
 
-// Function to display the region with the lowest number of accidents
-void showsLowest(string Region, int accidents) {
-    cout << "\nThe safest region is " << minRegion
-        << " with " << minAccidents << " reported accidents last year.\n";
-}
 
+void showsLowest(string Region, int accidents) {
+
+    // Using the local Region and accidents passed to this function
+    cout << "\nThe safest region is " << Region 
+        << " with " << accidents << " reported accidents last year.\n";
+}
 
 
     
